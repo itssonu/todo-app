616d5ca474a45e13fe8e984f9b89e2db8ea22053
itssonu <sonu98gupta@gmail.com>
2024-01-02 17:52:41 +0530

    sdvd

--- todo-application/src/components/todo-form/todo-form.jsx
+++ todo-application/src/components/todo-form/todo-form.jsx
@@ -6,16 +6,23 @@ import { usePostTodo } from '../../hooks/todos';
 export const TodoForm = () => {
   const { addTodo, setTodos, todos } = useContext(TodoApiContext);
   const [task, setTask] = useState('');
+  const [error, setError] = useState('');
   const { loading: deleteLoading, error: deleteError, postTodoData, response: postResponse } = usePostTodo(); // Use the delete hook
 
   useEffect(() => {
     console.log(postResponse ,'postResponse');
     setTodos([...todos, postResponse])
     setTask('')
+    setError('')
   }, [postResponse])
   
   const handleAddTodo = async () => {
-   postTodoData(task)
+    if (!task) {
+      setError('task is required')
+    }else{
+      postTodoData(task)
+    }
+   
   };
 
   const handleKeyUp = (e) => {
@@ -32,6 +39,9 @@ export const TodoForm = () => {
         onChange={(e) => setTask(e.target.value)}
         onKeyUp={handleKeyUp}
       />
+      {
+        error &&<span style={{color:'red'}}>{error}</span>
+      }
       <button type="button" onClick={handleAddTodo}>
         Add task
       </button>
